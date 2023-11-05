ccc69a89c50d2ad0efa89f79dad0a839dd1cbc91
Zainullin Damir <zainudam@fit.cvut.cz>
2023-11-05 16:36:41 +0100

    Add sort for biflows(avg 0,27s)

--- storage/cache.cpp
+++ storage/cache.cpp
@@ -635,25 +635,51 @@ uint32_t NHTFlowCache<NEED_FLOW_CACHE_STATS>::toeplitz_hash(const Packet& pkt) c
     rte_thash_tuple tuple = {0};
     if (pkt.ip_version == IP::v4){
         //rte_ipv4_tuple  orig ;
-        tuple.v4.src_addr = pkt.src_ip.v4;
-        tuple.v4.dst_addr = pkt.dst_ip.v4;
-        tuple.v4.sport = pkt.src_port;
-        tuple.v4.dport = pkt.dst_port;
+        if (!m_split_biflow){
+            if (pkt.src_ip.v4 <= pkt.dst_ip.v4){
+                tuple.v4.src_addr = pkt.src_ip.v4;
+                tuple.v4.dst_addr = pkt.dst_ip.v4;
+                tuple.v4.sport = pkt.src_port;
+                tuple.v4.dport = pkt.dst_port;
+            }else{
+                tuple.v4.src_addr = pkt.dst_ip.v4;
+                tuple.v4.dst_addr = pkt.src_ip.v4;
+                tuple.v4.sport = pkt.dst_port;
+                tuple.v4.dport = pkt.src_port;
+            }
+        }else{
+            tuple.v4.src_addr = pkt.src_ip.v4;
+            tuple.v4.dst_addr = pkt.dst_ip.v4;
+            tuple.v4.sport = pkt.src_port;
+            tuple.v4.dport = pkt.dst_port;
+        }
         return rte_softrss_bex((uint32_t*)&tuple.v4, RTE_THASH_V4_L3_LEN,(uint8_t*)m_rss_key);
     }else if (pkt.ip_version == IP::v6){
         //m_rss_key;
         rte_ipv6_hdr hdr = {0};
-
-        memcpy(hdr.src_addr, pkt.src_ip.v6,16);
-        memcpy(hdr.dst_addr, pkt.dst_ip.v6,16);
-
-        rte_thash_load_v6_addrs(&hdr,(rte_thash_tuple*)&tuple.v6);
-        tuple.v6.sport = pkt.src_port;
-        tuple.v6.dport = pkt.dst_port;
+        if (!m_split_biflow){
+            if (strncmp((const char*)pkt.src_ip.v6,(const char*)pkt.dst_ip.v6,16) <= 0){
+                memcpy(hdr.src_addr, pkt.src_ip.v6,16);
+                memcpy(hdr.dst_addr, pkt.dst_ip.v6,16);
+                rte_thash_load_v6_addrs(&hdr,(rte_thash_tuple*)&tuple.v6);
+                tuple.v6.sport = pkt.src_port;
+                tuple.v6.dport = pkt.dst_port;
+            }else{
+                memcpy(hdr.src_addr, pkt.dst_ip.v6,16);
+                memcpy(hdr.dst_addr, pkt.src_ip.v6,16);
+                rte_thash_load_v6_addrs(&hdr,(rte_thash_tuple*)&tuple.v6);
+                tuple.v6.sport = pkt.dst_port;
+                tuple.v6.dport = pkt.src_port;
+            }
+        }else{
+            memcpy(hdr.src_addr, pkt.src_ip.v6,16);
+            memcpy(hdr.dst_addr, pkt.dst_ip.v6,16);
+            rte_thash_load_v6_addrs(&hdr,(rte_thash_tuple*)&tuple.v6);
+            tuple.v6.sport = pkt.src_port;
+            tuple.v6.dport = pkt.dst_port;
+        }
         return rte_softrss_bex((uint32_t*)&tuple.v6, RTE_THASH_V6_L3_LEN,(uint8_t*)m_rss_key);
     }
-    //rte_thash_tuple targ;
-    //rte_thash_load_v6_addrs(&orig, &targ);
     return 666;
 }
 template<bool NEED_FLOW_CACHE_STATS>
