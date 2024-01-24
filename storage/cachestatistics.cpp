481390d90ed6c94730824c1a14045b243d942178
Damir Zainullin <zad_samsa@mail.ru>
2024-01-24 14:45:14 +0100

    Add cache statistics struct

---
+++ storage/cachestatistics.cpp
@@ -0,0 +1,8 @@
+//
+// Created by zaida on 24.01.2024.
+//
+
+#include "cachestatistics.hpp"
+
+namespace ipxp {
+} // namespace ipxp
---
+++ storage/cachestatistics.hpp
@@ -0,0 +1,14 @@
+//
+// Created by zaida on 24.01.2024.
+//
+
+#ifndef IPFIXPROBE_CACHE_CACHESTATISTICS_HPP
+#define IPFIXPROBE_CACHE_CACHESTATISTICS_HPP
+
+namespace ipxp {
+
+struct CacheStatistics {};
+
+} // namespace ipxp
+
+#endif // IPFIXPROBE_CACHE_CACHESTATISTICS_HPP
