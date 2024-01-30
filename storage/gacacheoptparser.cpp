10a3a9f34b0263ee368005d81ca030c1cb9d09b6
Damir Zainullin <zad_samsa@mail.ru>
2024-01-30 14:09:29 +0100

    Add options parser for GA cache.

---
+++ storage/gacacheoptparser.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zaida on 26.01.2024.
+//
+
+#include "gacacheoptparser.hpp"
+
+namespace ipxp {
+
+GACacheOptParser::GACacheOptParser(): CacheOptParser("gacache","Storage plugin implemented as a hash table, every row is managed by genetic algorithm"){
+    register_options();
+}
+GACacheOptParser::GACacheOptParser(const char* name, const char* description ): CacheOptParser(name,description){
+    register_options();
+}
+void GACacheOptParser::register_options(){
+    register_option("ic", "inputconfig", "PATH", "Path to the genetic configuration file", [this](const char *arg){m_infilename = arg; return true;}, OptionFlags::RequiredArgument);
+}
+} // namespace ipxp
---
+++ storage/gacacheoptparser.hpp
@@ -0,0 +1,24 @@
+//
+// Created by zaida on 26.01.2024.
+//
+
+#ifndef IPFIXPROBE_CACHE_GACACHEOPTPARSER_HPP
+#define IPFIXPROBE_CACHE_GACACHEOPTPARSER_HPP
+
+#include <string>
+#include "cacheoptparser.hpp"
+
+namespace ipxp {
+
+class GACacheOptParser : public CacheOptParser {
+public:
+    GACacheOptParser();
+    GACacheOptParser(const char* name, const char* description);
+    std::string m_infilename = "";
+private:
+    void register_options();
+};
+
+} // namespace ipxp
+
+#endif // IPFIXPROBE_CACHE_GACACHEOPTPARSER_HPP
