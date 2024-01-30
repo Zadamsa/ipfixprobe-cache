6e870d480b9ebc29fd98d2151eb887bb5b09d0ce
Damir Zainullin <zad_samsa@mail.ru>
2024-01-30 14:07:10 +0100

    Add genetic configuration class

---
+++ storage/gaconfiguration.cpp
@@ -0,0 +1,131 @@
+//
+// Created by zaida on 26.01.2024.
+//
+
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <ipfixprobe/plugin.hpp>
+#include "gaconfiguration.hpp"
+
+using namespace std::string_literals;
+namespace ipxp {
+
+GAConfiguration::GAConfiguration(uint32_t line_size): m_line_size(line_size){
+    uint32_t prev = 0;
+    uint32_t generated_nodes_count = 0;
+    for( uint32_t i = 0; i < line_size/4; i++){
+        std::uniform_int_distribution<std::mt19937::result_type> moves_dist(prev,generated_nodes_count);
+        uint8_t count = m_count_dist(m_rng);
+        uint8_t target = moves_dist(m_rng);
+        generated_nodes_count += count;
+        prev = target;
+        m_moves.push_back(MoveTuple{count,target});
+    }
+    mutate_insert_pos(1);
+    fix_counts();
+    fix_targets();
+}
+void GAConfiguration::fix() noexcept{
+    fix_counts();
+    fix_targets();
+}
+
+GAConfiguration::GAConfiguration(){};
+
+GAConfiguration GAConfiguration::mutate() const{
+    GAConfiguration new_configuration = *this;
+    while(new_configuration == *this) {
+        new_configuration.mutate_counts(0.2);
+        new_configuration.fix_counts();
+        new_configuration.mutate_targets(0.2);
+        new_configuration.fix_targets();
+        new_configuration.mutate_insert_pos(0.2);
+    }
+    return new_configuration;
+}
+void GAConfiguration::mutate_insert_pos(float probability) noexcept{
+    if (roll(probability))
+        m_insert_pos = m_insert_dist(m_rng);
+}
+void GAConfiguration::mutate_counts(float probability){
+    std::transform(m_moves.begin(), m_moves.end(), m_moves.begin(),[this,probability](MoveTuple& mp) {
+        return roll(probability) ? MoveTuple{(uint8_t)m_count_dist(m_rng),mp.m_value} : mp;
+    });
+}
+
+void GAConfiguration::read_from_file(const std::string& filename){
+    std::ifstream ifs(filename,std::ios::binary);
+    if (!ifs)
+        throw PluginError("Can't open GA configuration file: " + filename);
+    uint32_t in_line_size;
+    ifs.read((char*)&in_line_size,sizeof(in_line_size));
+    if (in_line_size != m_line_size)
+        throw PluginError("Invalid GA configuration line length. Config = "s + std::to_string(in_line_size) + ", Cache = " + std::to_string(m_line_size));
+    ifs.read((char*)&m_insert_pos,sizeof(m_insert_pos));
+    for(uint32_t i = 0; i < m_line_size/4; i++)
+        ifs.read((char*)&m_moves[i],sizeof(m_moves[0]));
+    if (!ifs)
+        throw PluginError("Invalid GA configuration file: " + filename);
+}
+
+void GAConfiguration::write_to_file(const std::string& filename) const {
+    std::ofstream ofs(filename,std::ios::binary);
+    if (!ofs)
+        throw PluginError("Can't open GA configuration savefile: " + filename);
+    ofs.write((char*)&m_line_size,sizeof(m_line_size));
+    ofs.write((char*)&m_insert_pos,sizeof(m_insert_pos));
+    for(uint32_t i = 0; i < m_line_size/4; i++)
+        ofs.write((char*)&  m_moves[i],sizeof(m_moves[0]));
+    if (!ofs)
+        throw PluginError("Can't save to GA configuration file: " + filename);
+}
+
+void GAConfiguration::mutate_targets(float probability){
+    uint32_t max = 0;
+    std::transform(m_moves.begin(), m_moves.end(), m_moves.begin(),[this,probability,&max](MoveTuple& mp) {
+        std::uniform_int_distribution<std::mt19937::result_type> dist(0,max);
+        if (roll(probability))
+            mp.m_value = dist(m_rng);
+        //if (mp.m_count >= max)
+        //    mp.m_value = max;
+        max += mp.m_count;
+        return mp;
+    });
+}
+
+void GAConfiguration::fix_counts() noexcept{
+    while (uint8_t diff = std::accumulate(m_moves.begin(), m_moves.end(), 0,[](uint8_t sum, const MoveTuple& mp) { return sum + mp.m_count; }) -  m_line_size)
+        m_moves[m_pair_dist(m_rng)].m_count += diff > 0 ? 1 : -1;
+}
+void GAConfiguration::fix_targets() noexcept{
+    for( uint32_t i = 1; i < m_moves.size(); i++)
+        if (m_moves[i-1].m_value + m_moves[i-1].m_count * m_moves[i-1].m_increment > m_moves[i].m_value) {
+            if (m_moves[i - 1].m_increment)
+                m_moves[i].m_value = m_moves[i - 1].m_value + m_moves[i - 1].m_count * m_moves[i - 1].m_increment;
+            else
+                std::swap(m_moves[i].m_value, m_moves[i - 1].m_value);
+        }
+}
+
+bool GAConfiguration::roll(double probability){
+    if (probability > 1 || probability < 0)
+        throw PluginError("Probability is not inside [0;1]");
+
+    return m_probability_dist(m_rng)/1000.0 <= probability;
+}
+
+bool GAConfiguration::operator==(const GAConfiguration& o) const noexcept{
+    return m_moves == o.m_moves && m_insert_pos == o.m_insert_pos;
+}
+bool GAConfiguration::operator!=(const GAConfiguration& o) const noexcept{
+    return !(*this == o);
+}
+std::pair<uint32_t,std::vector<uint32_t>> GAConfiguration::unpack() const noexcept{
+    std::vector<uint32_t> res;
+    for(const auto& mt : m_moves)
+        for(uint32_t i = 0; i < mt.m_count; i++)
+            res.push_back(mt.m_value + ( mt.m_increment ? i : 0 ) );
+    return {m_insert_pos,res};
+}
+} // namespace ipxp
---
+++ storage/gaconfiguration.hpp
@@ -0,0 +1,44 @@
+//
+// Created by zaida on 26.01.2024.
+//
+
+#ifndef IPFIXPROBE_CACHE_GACONFIGURATION_HPP
+#define IPFIXPROBE_CACHE_GACONFIGURATION_HPP
+
+#include <cstdint>
+#include <random>
+#include "movetuple.hpp"
+
+namespace ipxp {
+class GAConfiguration{
+public:
+    GAConfiguration(uint32_t line_size);
+    GAConfiguration();
+    GAConfiguration mutate() const;
+    void read_from_file(const std::string& filename);
+    void write_to_file(const std::string& filename) const;
+    void mutate_counts(float probability);
+    void mutate_targets(float probability);
+    void mutate_insert_pos(float probability) noexcept;
+    void fix() noexcept;
+    void fix_counts() noexcept;
+    void fix_targets() noexcept;
+    bool roll(double probability);
+    bool operator==(const GAConfiguration& o) const noexcept;
+    bool operator!=(const GAConfiguration& o) const noexcept;
+    std::pair<uint32_t,std::vector<uint32_t>> unpack() const noexcept;
+private:
+    std::vector<MoveTuple> m_moves;
+    uint16_t m_insert_pos = 0;
+    uint32_t m_line_size = 0;
+
+    std::mt19937 m_rng = std::mt19937(std::random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> m_probability_dist = std::uniform_int_distribution<std::mt19937::result_type>(0,1000);
+    std::uniform_int_distribution<std::mt19937::result_type> m_pair_dist = std::uniform_int_distribution<std::mt19937::result_type>(0,m_line_size/4 - 1);
+    std::uniform_int_distribution<std::mt19937::result_type> m_count_dist = std::uniform_int_distribution<std::mt19937::result_type>(1,m_line_size/4 );
+    std::uniform_int_distribution<std::mt19937::result_type> m_insert_dist = std::uniform_int_distribution<std::mt19937::result_type>(0,m_line_size - 1);
+};
+
+} // namespace ipxp
+
+#endif // IPFIXPROBE_CACHE_GACONFIGURATION_HPP
