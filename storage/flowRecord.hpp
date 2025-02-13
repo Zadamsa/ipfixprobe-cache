c82006e3847becb4ee1ccb26cf57d8d8ec81e068
Damir Zainullin <zaidamilda@gmail.com>
2025-02-13 14:02:58 +0100

    ++ DELETE

--- storage/flowRecord.hpp
+++ storage/flowRecord.hpp
@@ -29,6 +29,7 @@
 #include <ipfixprobe/flowifc.hpp>
 #include <cstdint>
 #include <optional>
+#include <cstring>
 
 namespace ipxp {
 
@@ -46,12 +47,6 @@ public:
     std::optional<OffloadMode> offload_mode;        /**< Offload mode of the flow. */
 #endif /* WITH_CTT */
 
-    FlowRecord();
-    ~FlowRecord();
-
-    void erase();
-    void reuse();
-
     __attribute__((always_inline)) bool is_empty() const noexcept
     {
         return m_hash == 0;
@@ -67,8 +62,128 @@ public:
         return hash == m_hash && m_flow.vlan_id == vlan_id;
     }
 
-    void create(const Packet &pkt, uint64_t pkt_hash);
-    void update(const Packet &pkt);
+ __attribute__((always_inline))   FlowRecord()
+{
+   erase();
 };
+
+__attribute__((always_inline)) ~FlowRecord()
+{
+   erase();
+};
+
+   __attribute__((always_inline))
+void erase()
+{
+   m_flow.remove_extensions();
+   m_hash = 0;
+   memset(&m_flow.time_first, 0, sizeof(m_flow.time_first));
+   memset(&m_flow.time_last, 0, sizeof(m_flow.time_last));
+   m_flow.ip_version = 0;
+   m_flow.ip_proto = 0;
+   memset(&m_flow.src_ip, 0, sizeof(m_flow.src_ip));
+   memset(&m_flow.dst_ip, 0, sizeof(m_flow.dst_ip));
+   m_flow.src_port = 0;
+   m_flow.dst_port = 0;
+   m_flow.src_packets = 0;
+   m_flow.dst_packets = 0;
+   m_flow.src_bytes = 0;
+   m_flow.dst_bytes = 0;
+   m_flow.src_tcp_flags = 0;
+   m_flow.dst_tcp_flags = 0;
+#ifdef WITH_CTT
+   is_waiting_for_export = false;
+   is_in_ctt = false;
+   offload_mode = std::nullopt;
+#endif /* WITH_CTT */
+}
+
+   __attribute__((always_inline))
+void reuse()
+{
+   m_flow.remove_extensions();
+   m_flow.time_first = m_flow.time_last;
+   m_flow.src_packets = 0;
+   m_flow.dst_packets = 0;
+   m_flow.src_bytes = 0;
+   m_flow.dst_bytes = 0;
+   m_flow.src_tcp_flags = 0;
+   m_flow.dst_tcp_flags = 0;
+#ifdef WITH_CTT
+   is_waiting_for_export = false;
+   is_in_ctt = false;
+   offload_mode = std::nullopt;
+#endif /* WITH_CTT */
+}
+   __attribute__((always_inline))
+void create(const Packet &pkt, uint64_t hash)
+{
+   m_flow.src_packets = 1;
+
+   m_hash = hash;
+
+   m_flow.time_first = pkt.ts;
+   m_flow.time_last = pkt.ts;
+   m_flow.flow_hash = hash;
+   m_flow.vlan_id = pkt.vlan_id;
+
+   memcpy(m_flow.src_mac, pkt.src_mac, 6);
+   memcpy(m_flow.dst_mac, pkt.dst_mac, 6);
+
+   if (pkt.ip_version == IP::v4) {
+      m_flow.ip_version = pkt.ip_version;
+      m_flow.ip_proto = pkt.ip_proto;
+      m_flow.src_ip.v4 = pkt.src_ip.v4;
+      m_flow.dst_ip.v4 = pkt.dst_ip.v4;
+      m_flow.src_bytes = pkt.ip_len;
+   } else if (pkt.ip_version == IP::v6) {
+      m_flow.ip_version = pkt.ip_version;
+      m_flow.ip_proto = pkt.ip_proto;
+      memcpy(m_flow.src_ip.v6, pkt.src_ip.v6, 16);
+      memcpy(m_flow.dst_ip.v6, pkt.dst_ip.v6, 16);
+      m_flow.src_bytes = pkt.ip_len;
+   }
+
+   if (pkt.ip_proto == IPPROTO_TCP) {
+      m_flow.src_port = pkt.src_port;
+      m_flow.dst_port = pkt.dst_port;
+      m_flow.src_tcp_flags = pkt.tcp_flags;
+   } else if (pkt.ip_proto == IPPROTO_UDP) {
+      m_flow.src_port = pkt.src_port;
+      m_flow.dst_port = pkt.dst_port;
+   } else if (pkt.ip_proto == IPPROTO_ICMP ||
+      pkt.ip_proto == IPPROTO_ICMPV6) {
+      m_flow.src_port = pkt.src_port;
+      m_flow.dst_port = pkt.dst_port;
+   }
+#ifdef WITH_CTT
+   is_waiting_for_export = false;
+   is_in_ctt = false;
+#endif /* WITH_CTT */
+}
+   
+   __attribute__((always_inline))
+void update(const Packet &pkt)
+{
+   m_flow.time_last = pkt.ts;
+   if (pkt.source_pkt) {
+      m_flow.src_packets++;
+      m_flow.src_bytes += pkt.ip_len;
+
+      if (pkt.ip_proto == IPPROTO_TCP) {
+         m_flow.src_tcp_flags |= pkt.tcp_flags;
+      }
+   } else {
+      m_flow.dst_packets++;
+      m_flow.dst_bytes += pkt.ip_len;
+
+      if (pkt.ip_proto == IPPROTO_TCP) {
+         m_flow.dst_tcp_flags |= pkt.tcp_flags;
+      }
+   }
+}
+};
+
+
 
 } // ipxp
